e5155cdfabcc0ab1df264db155cc16914cd2b3e2
Akechi001 <enricoarianthous@gmail.com>
2024-01-13 01:26:53 +0800

    Add Router for move to another pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import './App.css'
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Login from './Displays/Login/Login.jsx'
+import HomePage from "./Displays/Home/HomePage.jsx";
 
 function App() {
   return (
-    <>
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Login/>}/>
+          <Route path='/home' element={<HomePage/>}/>
+        </Routes>
+      </Router>
 
-    </>
   )
 }
 
