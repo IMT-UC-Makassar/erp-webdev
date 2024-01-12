7b3248a6ce8bdbc5c66c539f9a1b5a0715ae237d
Akechi001 <enricoarianthous@gmail.com>
2024-01-13 01:29:15 +0800

    arrange file and folder

---
+++ src/Displays/Home/HomePage.jsx
@@ -0,0 +1,14 @@
+import '../../Styles/App.css'
+import Header from "../../Components/Header/header.jsx";
+
+
+function HomePage() {
+    return(
+        <>
+            <Header />
+            <p>Home page</p>
+        </>
+    )
+}
+
+export default HomePage;
