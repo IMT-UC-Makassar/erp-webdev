6bd930c1094c545050a5356e25cfab2e59eeaaec
Akechi001 <enricoarianthous@gmail.com>
2024-01-13 22:23:43 +0800

    Add Page not Found Pages

--- src/Displays/Home/HomePage.jsx
+++ src/Displays/Home/HomePage.jsx
@@ -1,12 +1,15 @@
 import '../../Styles/App.css'
 import Header from "../../Components/Header/header.jsx";
 import Footer from "../../Components/Footer/footer.jsx";
-
+import Menu from "../../Components/HamburgerMenu/menu.jsx"
+import NotFound from "../../Components/ErrorPage/Error.jsx"
 
 function HomePage() {
     return(
         <>
             <Header />
+            <Menu/>
+            <NotFound/>
             <p>Home page</p>
             <Footer />
         </>
