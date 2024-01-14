ab5e468f8fbd649610629969e56494dd4fc4507f
Akechi001 <enricoarianthous@gmail.com>
2024-01-14 17:42:49 +0800

    Hamburger menu and add paths of router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Login from './Displays/Login/Login.jsx'
 import HomePage from "./Displays/Home/HomePage.jsx";
-
+import NotFoundPage from "./Displays/NotFound/Error.jsx"
+import InvenroryList from "./Displays/Inventory/InvenroryList.jsx";
+import MeetingList from "./Displays/Meeting/MeetingLIst.jsx";
+import MeetingDetailed from "./Displays/Meeting/MeetingDetailed.jsx";
+import Profile from "./Displays/Profile/Profile.jsx";
+import ProjectDetailed from "./Displays/Project/ProjectDetailed.jsx";
+import ProjectList from "./Displays/Project/ProjectList.jsx";
+import Finance from "./Displays/Finance/Finance.jsx";
 function App() {
   return (
       <Router>
         <Routes>
           <Route exact path='/' element={<Login/>}/>
           <Route path='/home' element={<HomePage/>}/>
+          <Route path='/404' element={<NotFoundPage/>}/>
+          <Route path='/inventorylist' element={<InvenroryList/>}/>
+          <Route path='/meetinglist' element={<MeetingList/>}/>
+          <Route path='/meetinglist/meetingdetailed' element={<MeetingDetailed/>}/>
+          <Route path='/profile' element={<Profile/>}/>
+          <Route path='/projectlist' element={<ProjectList/>}/>
+          <Route path='/projectlist/projectDetailed' element={<ProjectDetailed/>}/>
+          <Route path='/finance' element={<Finance/>}/>
         </Routes>
       </Router>
 
