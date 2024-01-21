fba8ab90266b584838413a619f24a474ff66bdf4
Antonius Indra Dharma Prasetya <anindhpra@gmail.com>
2024-01-22 07:57:48 +0800

    change how the auth check is performed

--- src/services/privateRoute.jsx
+++ src/services/privateRoute.jsx
@@ -1,26 +1,25 @@
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import {useEffect} from 'react';
+import {useNavigate} from 'react-router-dom';
 import {useAuth} from "./authContext.jsx";
 import createAxiosInstance from "../services/axiosInstance";
 
-function PrivateRoute({ children }) {
-    const { isAuthenticated, login, logout } = useAuth();
+function PrivateRoute({children}) {
+    const {isAuthenticated, login, logout} = useAuth();
     const navigate = useNavigate();
 
     useEffect(() => {
         if (!isAuthenticated) {
             const token = localStorage.getItem('token');
             if (token) {
-                console.log(token);
                 const axiosInstance = createAxiosInstance(true);
-                    axiosInstance.get('/auth')
-                        .then(() => {
-                            login(token);
-                        }).catch(() => {
-                            logout();
+                axiosInstance.get('/auth')
+                    .then(() => {
+                        login(token);
+                    })
+                    .catch(() => {
+                        logout();
                         navigate('/');
                     });
-
             } else {
                 navigate('/');
             }
