47e2d949f8267d6d9a7ecd0e5c30f01aad8ba3a2
Huoammmmm <115942905+Huoammmmm@users.noreply.github.com>
2024-01-21 13:23:38 +0800

    Buat Form add new meeting

--- src/Displays/Meeting/MeetingLIst.jsx
+++ src/Displays/Meeting/MeetingLIst.jsx
@@ -2,6 +2,7 @@ import '../../Styles/App.css'
 import Header from "../../Components/Header/header.jsx";
 import Footer from "../../Components/Footer/footer.jsx";
 import Menu from "../../Components/HamburgerMenu/menu.jsx"
+import AddNewMeeting from './AddNewMeeting.jsx';
 
 function MeetingList(){
     return(
@@ -10,6 +11,7 @@ function MeetingList(){
             <Menu/>
             <p>Meeting List</p>
             <Footer />
+            <AddNewMeeting></AddNewMeeting>
         </>
     )
 }
