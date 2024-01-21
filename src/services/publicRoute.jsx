c6b5cf24e14ae1886012489bf36b4227b167a80a
Antonius Indra Dharma Prasetya <anindhpra@gmail.com>
2024-01-21 17:14:52 +0800

    typo

--- src/services/publicRoute.jsx
+++ src/services/publicRoute.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useAuth } from './AuthContext';
 import createAxiosInstance from "./axiosInstance.jsx";
+import {useAuth} from "./authContext.jsx";
 
 function PublicRoute({ children }) {
     const { isAuthenticated, login } = useAuth();
